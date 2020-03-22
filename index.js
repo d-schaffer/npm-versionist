fb8f6077268ed433b8efe1ae8ef46286333fb812
dschaffer <41639506+dev-schaffer@users.noreply.github.com>
2020-03-22 11:06:52 +0100

    add console output for github actions

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ const github = require('@actions/github');
 
 console.log("Verstions: ");
 
-var gnv = require('./file2.js');
+/*var gnv = require('./file2.js');
 
-gnv();
+gnv();*/ 
+
+const execSync = require('child_process').execSync;
+// import { execSync } from 'child_process';  // replace ^ if using ES modules
+const output = execSync('npm list --depth=0', { encoding: 'utf-8' });  // the default is 'buffer'
+// console.log('Output was:\n', output);
+
+let stringi = output.split("\n");
+let packages = [];
+// console.log(length(stringi));
+stringi.forEach(function(element) { 
+    console.log(element)
+    element = element.replace("+--", "");
+    // element = element.replace(/[0-9]/g,"");
+    // element = element.replace(/\./g,"");
+    element = element.replace('@',":");
+    if (element != ''){
+        packages.push(element.trim());
+    }
+});
+
+console.log(packages[1]);
+core.setOutput("Hallooo TEST");
